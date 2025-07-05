188aa043ae743fc2306ec5d34b8c8211c6886581
Bryan Dela Cruz <137765730+Bryan652@users.noreply.github.com>
2025-07-05 14:40:08 -0400

    first commit

---
+++ app/Livewire/Test.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Test extends Component
+{
+    public function render()
+    {
+        return <<<'HTML'
+        <div>
+            {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+        </div>
+        HTML;
+    }
+}
